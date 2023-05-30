80ee014fb8c7db0c4a66beeff08b6ee7f3d1197d
Kristen <kristenmarcinek@protonmail.com>
2023-05-30 15:16:15 -0400

    lesbian flag and black lives matter

--- blacklivesmatter/sketch.js
+++ blacklivesmatter/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("black");
+  textSize(200);
+  fill('white');
+  textAlign(CENTER);
+  textWrap(WORD);
+  text('BLACK', width / 2, height / 2 - 200);
+  text('LIVES', width / 2, height / 2);
+  text('MATTER', width / 2, height / 2 + 200);
+  filter(BLUR, 15);
+
+  text('BLACK', width / 2, height / 2 - 200);
+  text('LIVES', width / 2, height / 2);
+  text('MATTER', width / 2, height / 2 + 200);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  
 }
--- lesbianflag/sketch.js
+++ lesbianflag/sketch.js
@@ -1,7 +1,43 @@
+// Constants
+const Y_AXIS = 1;
+const X_AXIS = 2;
+let b1, b2, c1, c2;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // Define colors
+  b1 = color("orange");
+  b2 = color("white");
+  b3 = color("magenta");
+
+  noLoop();
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  // Background
+  setGradient(0, 0, width, height / 2, b1, b2, Y_AXIS);
+  setGradient(0, height / 2, width, height, b2, b3, Y_AXIS);
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
+  }
 }
